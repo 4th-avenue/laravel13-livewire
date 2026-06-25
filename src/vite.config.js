691e9c2f507d0ce289c3c945d58ebc24608735a0
mystee <quebec706@gmail.com>
2026-06-25 19:46:07 +0900

    Laravel Breeze (LIVEWIRE) 설치

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
     ],
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: true,
+        port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173,
+        hmr: {
+            host: 'localhost',
         },
     },
 });
