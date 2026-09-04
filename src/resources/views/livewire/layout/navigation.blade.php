e7a0aa2f9be5821714251d9c6b972f06c3051411
mystee <quebec706@gmail.com>
2026-09-04 13:08:50 +0900

    비로그인 사용자에게는 Dashboard 메뉴가 보이지 않도록 수정

--- src/resources/views/livewire/layout/navigation.blade.php
+++ src/resources/views/livewire/layout/navigation.blade.php
@@ -30,9 +30,11 @@ new class extends Component
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    @auth
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @endauth
                     <x-nav-link :href="route('articles.index')" :active="request()->routeIs('articles.*')" wire:navigate>
                         {{ __('Articles') }}
                     </x-nav-link>
@@ -120,9 +122,11 @@ new class extends Component
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            @auth
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            @endauth
             <x-responsive-nav-link :href="route('articles.index')" :active="request()->routeIs('articles.*')" wire:navigate>
                 {{ __('Articles') }}
             </x-responsive-nav-link>
