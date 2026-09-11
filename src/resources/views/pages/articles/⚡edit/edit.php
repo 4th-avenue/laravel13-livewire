f9d26fdecd4cfdf792f784b034f5ca910a1b8620
mystee <quebec706@gmail.com>
2026-09-11 23:56:12 +0900

    글 수정 기능 구현

--- src/resources/views/pages/articles/⚡edit/edit.php
+++ src/resources/views/pages/articles/⚡edit/edit.php
@@ -1,8 +1,42 @@
 <?php
 
+use App\Models\Article;
+use App\Models\Category;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public Article $article;
+
+    #[Validate('required|exists:categories,id')]
+    public $category_id;
+    #[Validate('required|string|max:60')]
+    public $title;
+    #[Validate('required|string|max:21844')]
+    public $body;
+
+    public $childCategories = [];
+
+    public function mount(Article $article)
+    {
+        $this->article = $article;
+
+        $this->fill($this->article->only(['category_id', 'title', 'body']));
+
+        $this->childCategories = Category::whereNotNull('parent_id')
+            ->pluck('name', 'id')
+            ->toArray();
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->article->update(
+            $this->only(['category_id', 'title', 'body'])
+        );
+
+        return $this->redirectRoute('articles.show', $this->article, navigate: true);
+    }
 };
--- src/resources/views/pages/articles/⚡show/show.blade.php
+++ src/resources/views/pages/articles/⚡show/show.blade.php
@@ -17,6 +17,12 @@
                         <hr class="my-3">
                         <p>{{$article->body}}</p>
                     </div>
+
+                    <div class="flex items-center justify-end mt-3">
+                        <a href="{{ route('articles.edit', $article) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:navigate>
+                            {{ __('Edit') }}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
