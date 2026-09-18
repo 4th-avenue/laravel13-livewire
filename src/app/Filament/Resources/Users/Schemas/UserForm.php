3b35daf3178d5cea924017784f7d582b86a5ac62
mystee <quebec706@gmail.com>
2026-09-18 20:39:34 +0900

    User 리소스의 UI 수정 및 비밀번호 필드를 새 User를 생성할 때만 필수로 지정

--- src/app/Filament/Resources/Users/Schemas/UserForm.php
+++ src/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -12,20 +13,28 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('userid')
-                    ->required(),
-                TextInput::make('nickname')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                DateTimePicker::make('email_verified_at'),
-                TextInput::make('photo_path')
-                    ->default(null),
-                TextInput::make('password')
-                    ->password()
-                    ->required(),
+                Section::make('User')
+                    ->schema([
+                        TextInput::make('userid')
+                            ->required()
+                            ->unique(),
+                        TextInput::make('nickname')
+                            ->required()
+                            ->unique(),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->required()
+                            ->unique(),
+                        DateTimePicker::make('email_verified_at'),
+                        TextInput::make('photo_path')
+                            ->default(null),
+                        TextInput::make('password')
+                            ->password()
+                            ->required(fn (string $operation): bool => $operation === 'create')
+                            ->dehydrated(fn (?string $state) => filled($state)),
+                    ])->columns(2)
+                    ->columnSpanFull()
             ]);
     }
 }
--- src/app/Filament/Resources/Users/Tables/UsersTable.php
+++ src/app/Filament/Resources/Users/Tables/UsersTable.php
@@ -14,6 +14,7 @@ class UsersTable
     {
         return $table
             ->columns([
+                TextColumn::make('id'),
                 TextColumn::make('userid')
                     ->searchable(),
                 TextColumn::make('nickname')
