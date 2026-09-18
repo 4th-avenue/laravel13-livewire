d6a2af5f1228eb03c06e719841e52f538ae78801
mystee <quebec706@gmail.com>
2026-09-18 13:14:16 +0900

    filament User 리소스 생성

---
+++ src/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('userid')
+                    ->required(),
+                TextInput::make('nickname')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('photo_path')
+                    ->default(null),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
