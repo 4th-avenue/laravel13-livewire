bd51314315f8ca753511b0d0617076b9a9f5ae39
mystee <quebec706@gmail.com>
2026-09-21 14:49:35 +0900

    역할(Role)에 권한(Permission) 할당 기능 구현

--- src/app/Filament/Resources/Roles/Schemas/RoleForm.php
+++ src/app/Filament/Resources/Roles/Schemas/RoleForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Roles\Schemas;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
@@ -18,7 +19,12 @@ class RoleForm
                             ->minLength(2)
                             ->maxLength(30)
                             ->required()
-                            ->unique()
+                            ->unique(),
+                        Select::make('permissions')
+                            ->placeholder('Select an option')
+                            ->multiple()
+                            ->relationship(titleAttribute: 'name')
+                            ->preload(),
                     ])
                     ->columnSpanFull()
             ]);
