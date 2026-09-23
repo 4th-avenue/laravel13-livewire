46c59b73616aed9db45fe755d5271984b08e31bc
mystee <quebec706@gmail.com>
2026-09-23 12:32:31 +0900

    회원 가입하면 자동으로 user 역할 부여 및 랜덤으로 새 User 만들기 기능 구현

--- src/app/Filament/Resources/Users/Pages/ListUsers.php
+++ src/app/Filament/Resources/Users/Pages/ListUsers.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use App\Models\User;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -13,7 +15,15 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->label('계정 생성'),
+            Action::make('create_random_user')
+                ->label('랜덤 계정 생성')
+                ->action(function () {
+                    User::factory()->create([
+                        'remember_token' => null,
+                    ])->assignRole('user');
+                }),
         ];
     }
 }
